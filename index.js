09f604ac79775f158c9ceb2891f2fdc15726c227
dheeraj singh <dheerajsingh86696@gmail.com>
2024-04-28 22:19:34 +0530

    Update index.js
    comment
    
    Co-Authored-By: Mohd Ajlal <ajlal00786@gmail.com>

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {v2 as cloudinary} from 'cloudinary';
 dotenv.config();
 
 
-
+// we will add cloudinary configuration here soon
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
